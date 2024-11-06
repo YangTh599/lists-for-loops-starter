fe07aca1516b8704d5b3cc7e640ee9d5779b2703
YangTh599 <YangTh599@northwested.org>
2024-11-06 10:29:23 -0500

    did assignment

---
+++ main.py
@@ -0,0 +1,26 @@
+# Thayer Yang
+# 6 NOV 2024
+# For Loop Practice
+
+from time import sleep
+
+# 4-1 Pizzas
+
+favorite_pizzas = ['deep dish', 'cheese', 'jalapenos']
+
+for pizza in favorite_pizzas:
+    print(f'I enjoy {pizza} pizza!')
+    sleep(.2)
+print('I really love pizza!')
+
+# 4-2 Animals
+
+print()
+
+animals = ['Red panda', 'Giant panda', 'Tibetan antelope', 'Caracal']
+
+for animal in animals:
+    print(f'{animal}s are cool animals in Asia.')
+    sleep(.2)
+
+print('Any of these animals can be found in Asia.')
